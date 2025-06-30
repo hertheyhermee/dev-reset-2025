0ce55cf35e68e6535b5e46188e176f8243cd441e
hertheyhermee <tiamiyuhayzed@gmail.com>
2025-06-30 14:06:13 +0100

    register endpoint, hashing passwords and comparing

--- node/auth-api/routes/auth.routes.js
+++ node/auth-api/routes/auth.routes.js
@@ -1,25 +1,59 @@
 import { Router } from 'express'
 import jwt from 'jsonwebtoken'
 import { authenticate } from '../middleware/auth.middleware.js';
+import bcrypt from 'bcrypt'
 
 const router = Router();
 
-router.post('/login', (req, res) => {
+const users = [{ email: "test123@gmail.com", password: '$2b$10$H56hHnglXKzyOVj/ERFj5ukSZLKQCgUMf51b7cy5tNAq7NPW0nRqy' }]
+
+router.post('/register', async (req, res) => {
     const { email, password } = req.body
 
-    if(email === 'admin@example.com' && password === 'password123') {
-        const token = jwt.sign(
-            { email, role: 'admin' },
-            process.env.JWT_SECRET,
-            { expiresIn: '1h' }
-        )
-        return res.json({
-            message: 'Login successful',
-            token
-        })
+    if(!email || !password || password.length < 6) {
+        return res.status(400).json({ message: "Email and valid password required" })
     }
 
-    return res.status(401).json({ message: "Invalid credentials!" })
+    const existingUser = users.find(user => user.email === email)
+    if(existingUser)
+        return res.status(400).json({ message: "User already exists" })
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    users.push({ email, password: hashedPassword });
+
+    console.log(users)
+
+    return res.status(201).json({ message: "User registered successfully" })
+})
+
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body
+
+    console.log(users);
+    
+
+    const user = users.find(user => user.email === email)
+    
+    if(!user) {
+        return res.status(401).json({message: "Invalid email or password"})
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+    
+    if(!match) {
+        return res.status(401).json({ message: "Invalid credentials!" })
+    }
+    const token = jwt.sign(
+        { email, role: 'user' },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
+    )
+    return res.json({
+        message: 'Login successful',
+        token
+    })
+
 
 })
 
